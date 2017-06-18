ac4df4d58ce8012ca569bd14a39ed041b3cdbb65
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2017-06-18 09:20:40 +0530

    Added ABI decoding

--- TestChain/startgeth.bat
+++ TestChain/startgeth.bat
@@ -1,4 +1,7 @@
 RD /S /Q %~dp0\devChain\geth
 
 geth.exe --datadir=devChain init genesis_dev.json
-geth.exe --networkid=12345 --cache=2048 --maxpeers=0 --datadir=devChain --rpc --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console  
+geth.exe --mine --networkid=12345 --cache=2048 --maxpeers=0 --datadir=devChain --rpc --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console  
+
+REM geth.exe --rinkeby --syncmode "fast" --datadir=rinky --mine console
+REM geth.exe --testnet --syncmode "fast" --datadir=testnet --verbosity 0 console
